d1aaf0aee104edf2ffd93d09b68b61dda71499ce
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2019-09-17 10:02:50 -0600

    adding third days work

--- twitoff/app.py
+++ twitoff/app.py
@@ -8,14 +8,31 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
- 
+
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message = ''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets    
+            pass
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+       
     @app.route('/reset')
     def reset():
         DB.drop_all()
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -14,3 +14,32 @@ BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 
 # TODO write some useful functions 
+
+def add_or_update_user(username):
+    """ add or update a user *and* their tweets, error if no/private user.""""
+    """ If not private and no error, will pass to the else statement that commits the data  """"
+    try:
+        twitter_user = TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id) or 
+                   User(id=twitter_user.id, name=username))
+        DB.session.add(db_user)      
+        # We want as many recent nonretweet/reply statuses as we can get     
+        tweets = twitter_user.timeline(
+            count=200, exlude_replies=True, include_rts=False,
+            tweet_mode='extended', since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            # Get embedding for tweet, and store in db
+            embedding = BASILICA.embed_sentence(tweet.full_text,
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
+                             embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)                                          
+    except Exception as e:
+        print('Error Processing {} : {}'.format(username, e))
+        raise e
+    else:
+        # if no errors then commit to database
+        DB.session.commit()
